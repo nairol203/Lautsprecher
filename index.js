593aa16dfdeebd4342f15e36cb1f41ca55f8f55f
nairol <73656754+nairol203@users.noreply.github.com>
2021-02-16 12:48:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,32 +6,36 @@ const client = new Discord.Client();
 const elevatorMusic = require('./elevator-music');
 
 client.on('ready', async () => {
-	const messages = [
-		'Ufo361',
-		'Lil Peep',
-		'Edo Saiya',
-		'KASIMIR1441',
-		'Gzuz',
-		'Bonez MC',
-		'187 Strassenbande',
-		'The Weeknd',
-		'Juice WRLD',
-		'Freakso',
-		'Selphius',
-		'KD/A',
-		'TWICE',
-		'NF',
-		'Alligatoah',
-		'GReeeN',
-		'Rass Limit',
-		'Machine Gun Kelly',
-		'Our Last Night',
-	];
-	const randomArtist = messages[Math.floor(Math.random() * messages.length)];
-	client.user.setActivity(randomArtist, { type : 'LISTENING' });
 	elevatorMusic(client);
 	console.log(`${client.user.username} is online!`);
 
+	const setRandomArtist = () => {
+		const messages = [
+			'Ufo361',
+			'Lil Peep',
+			'Edo Saiya',
+			'KASIMIR1441',
+			'Gzuz',
+			'Bonez MC',
+			'187 Strassenbande',
+			'The Weeknd',
+			'Juice WRLD',
+			'Freakso',
+			'Selphius',
+			'KD/A',
+			'TWICE',
+			'NF',
+			'Alligatoah',
+			'GReeeN',
+			'Rass Limit',
+			'Machine Gun Kelly',
+			'Our Last Night',
+		];
+		const randomArtist = messages[Math.floor(Math.random() * messages.length)];
+		client.user.setActivity(randomArtist, { type : 'LISTENING' });
+		setTimeout(setRandomArtist, 1000 * 60 * 2);
+	};
+	setRandomArtist();
 });
 
 client.login(process.env.TOKEN);
